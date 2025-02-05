76834067979c41e92fc49442bc0539d28cecbe2d
BugHunter-X <askzenai@gmail.com>
2025-02-05 02:20:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Unexpected error handling
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
